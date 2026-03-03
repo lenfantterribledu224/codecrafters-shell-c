3413676d164837bd01c7d798ed402e1a25f97940
madaraUchiha <madarauchiha>
2026-03-03 11:39:30 -0600

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -3,12 +3,16 @@
 #include <string.h>
 
 int main(int argc, char *argv[]) {
-while(1){  // Flush after every printf
-  setbuf(stdout, NULL);
-  char command[1024];
+setbuf(stdout, NULL);
+char command[1024];
+while(1){
   printf("$ ");
   fgets(command, sizeof(command), stdin);
   command[strcspn(command, "\n")] = '\0';
-  printf("%s: command not found\n", command);}
+  if (strcmp(command, "exit") == 0) {
+  exit(0);
+}
+  printf("%s: command not found\n", command);
+}
   return 0;
 }
