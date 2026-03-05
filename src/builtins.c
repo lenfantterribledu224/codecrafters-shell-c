76ca8b28e81326c5261369445826cdb3833060af
madaraUchiha <madarauchiha>
2026-03-05 14:47:03 -0600

    codecrafters submit [skip ci]

--- src/builtins.c
+++ src/builtins.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "shell.h"
+#include <unistd.h>
 
 void do_echo(char *args) {
     printf("%s\n", args);
@@ -22,6 +23,27 @@ void do_type(char *args) {
             printf("%s is a shell builtin\n", args);
             return;
         }
+    }
+    // not a builtin, search PATH
+    char *path = getenv("PATH");
+    if (path == NULL) {
+        printf("%s: not found\n", args);
+        return;
+    }
+    char path_copy[1024];
+    strcpy(path_copy, path);
+
+    char *dir = strtok(path_copy, ":");
+    while (dir != NULL) {
+        char full_path[1024];
+        snprintf(full_path, sizeof(full_path), "%s/%s", dir, args);
+
+        if (access(full_path, X_OK) == 0) {
+            printf("%s is %s\n", args, full_path);
+            return;
+        }
+
+        dir = strtok(NULL, ":");
     }
     printf("%s: not found\n", args);
 }
